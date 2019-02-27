8d2cd8b45939c6c348e2c19cd1717f50a3872611
Deepdasolution <diplove32928@gmail.com>
2019-02-27 10:26:20 +0545

    First commit

--- Csv.java
+++ Csv.java
@@ -14,6 +14,12 @@ class Contact
 	private String email;
 	private String phone;
 	
+	Contact()
+	{
+		
+	}
+	
+	
 	public Contact(String name,String email,String phone)
 	{
 		this.name=name;
@@ -75,22 +81,93 @@ public class Csv {
 		}
 	}
 
-	public static void main(String[] args) {
-		ArrayList<Contact> contactList= new ArrayList<Contact>();
+	private static ArrayList<Contact> readCSV(String filename)
+	{
+		ArrayList<Contact> contacts=new ArrayList<Contact>();
 		
-		/*
-		 Contact ct1= new Contact("Ram","syys","1223");
-		 contactList.add(ct1)
-		 Yesai gari 5 ota gareni hunx
-		 * */
+		int count=0;
+		
+		int max=0;
+		int min=0;
+		
+		Contact max_ct= new Contact();
+		Contact min_ct= new Contact();
+		
+		
+		FileReader fr;
+		try {
+		fr= new FileReader(filename);
+		BufferedReader br= new BufferedReader(fr);
+		
+		String line="";
+		System.out.println("The Data in the File are");
+		System.out.println("------------------------------");
+		while((line=br.readLine())!=null)
+			{
+
+				System.out.println(line);
+				
+				String[] items= line.split(",");
+				Contact ct= new Contact(items[0], items[1], items[2]);
+				String phone= ct.getPhone();
+				String[] numbers=phone.split("");
+				int sum=0;
+				
+				for(String number:numbers)
+				{
+					int n=Integer.parseInt(number);
+					sum+=n;
+				}
+				
+				
+				if(count==0)
+				{
+					max=sum;
+					min=sum;
+					min_ct=ct;
+					max_ct=ct;
+					
+				}
+				else {
+					if(sum>max)
+					{
+						max=sum;
+						max_ct=ct;
+					}
+					if(sum<min)
+					{
+						min=sum;
+						min_ct=ct;
+					}
+				}
+				++count;
+				
+			}
+		System.out.println();
+		System.out.println("------------------------------");
+		System.out.printf("Max number %s is with %s and Email is %s\n",max_ct.getPhone(),max_ct.getName(),max_ct.getEmail());
+		System.out.printf("Min number %s is with %s and Email is %s",min_ct.getPhone(),min_ct.getName(),min_ct.getEmail());
+		
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+		return contacts;
+		
+	}
+	public static void main(String[] args) {
+
+		ArrayList<Contact> contactList= new ArrayList<Contact>();
 		contactList.add(new Contact("ram","ram@gmail.com","9847870251"));
 		contactList.add(new Contact("syam","syam@gmail.com","9847870252"));
 		contactList.add(new Contact("gita","gita@gmail.com","9847870253"));
 		contactList.add(new Contact("sita","sita@gmail.com","9847870254"));
 		contactList.add(new Contact("prerana","prerana@gmail.com","9847870255"));
 		
-		writeToCsv(contactList,"C:\\Users\\DELL\\Desktop\\Deep.csv");
+		writeToCsv(contactList,"Deep.csv");
 
+		readCSV("Deep.csv");
 	}
 
 }
